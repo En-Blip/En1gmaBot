df09ee9000562949143650ce888eb4bc41e8ac37
En1gmaUnknown <imfastasfrickboii@gmail.com>
2023-12-25 11:14:22 -0700

    first production code

--- En1gmaBot_v1.2.2.py
+++ En1gmaBot_v1.2.2.py
@@ -6,12 +6,15 @@ import sys
 import json
 import sqlite3
 import os
+import numpy as np
 
 '''
 CHANGELOG
-    added moderator perm checking
-    added a command to update the twitch schedule
-    temporarily removed sql function
+    saves now keeps track of the channels
+    auto qed for pencenters channel
+
+TODO
+    possible bug when checking saves for a user with no saves
 '''
 
 SCHEDULE = {
@@ -72,6 +75,9 @@ class Bot:
         # get spreadsheet values
         self.get_sheet_values()
 
+        # spreadsheet variables
+        self.CHANNEL_COLUMNS = ['dondoesmath', 'dannyhighway', 'etothe2ipi', 'pencenter', 'enstucky']
+
     def authorize_bot(self, bot_username):
         # Set up the connection to the IRC server
         irc_server = 'irc.chat.twitch.tv'
@@ -98,6 +104,7 @@ class Bot:
     def update_sheet_values(self):
         '''a function that updates the values in my bot google sheet, specifically configured for this sheet
         dependencies: save_sheet()'''
+
         # open the service account
         gc = gspread.service_account(filename=self.gspread_filename)
 
@@ -105,11 +112,11 @@ class Bot:
         # open and return data to the sheets
         command_outputs = spreadsheet.worksheet('Command Outputs') #Commands
         command_descriptions = spreadsheet.worksheet('Command Descriptions') # Command Descriptions
-        saves_counter = spreadsheet.worksheet('SavesCounter') #SavesCounter
+        saves_table = spreadsheet.worksheet('SavesCounter') #SavesCounter
 
+        
         command_outputs.clear()
         command_descriptions.clear()
-        saves_counter.clear()
 
         # update the sheets
         for i in range(len(self.command_descriptions.keys()), 0, -1):
@@ -120,9 +127,20 @@ class Bot:
             time.sleep(1)
             command_outputs.insert_row([list(self.default_responses.keys())[i-1], list(self.default_responses.values())[i-1]], 1)
         
+
         for i in range(len(self.saves_counter.keys()), 0, -1):
             time.sleep(1)
-            saves_counter.insert_row([list(self.saves_counter.keys())[i-1], list(self.saves_counter.values())[i-1]], 1)
+
+            # get the save values to update the table range, adding the total on the end
+            saves_values = [list(self.saves_counter.keys())[i-1]] + list(self.saves_counter.values())[i-1].tolist()
+
+            # update the table with the range of values
+            for j, val in enumerate(saves_values):
+                # get the letter of the cell
+                letter = chr(ord('A') + j)
+
+                # update the cell
+                saves_table.update(f'{letter}{2+i}', val)
 
         print('sheets successfully updated')
     
@@ -130,6 +148,9 @@ class Bot:
 
         #for channel_name in self.channel_names:
             #send_message(self.irc_socket, channel_name, 'fingie wingies everyone, En1gmaBot has arrived')
+
+        # store pencenters pop quiz answer
+        pq_ans = 'πa'
 
         try:
             # Continuously listen for messages in the chat
@@ -145,8 +166,10 @@ class Bot:
                     cleaned_response = clean_response(response)
                     if cleaned_response['mod_status']:
                         print(cleaned_response['channel_name'], '(mod)' + cleaned_response['username'] + ':', cleaned_response['message'])
+                    elif cleaned_response['vip_status']:
+                        print(cleaned_response['channel_name'], '(vip)' + cleaned_response['username'] + ':', cleaned_response['message'])
                     else:
-                        print('via', cleaned_response['channel_name'], cleaned_response['username'] + ':', cleaned_response['message'])
+                        print(cleaned_response['channel_name'], cleaned_response['username'] + ':', cleaned_response['message'])
 
                     # sync chat messages between channels
                     if len(self.synced_channels) > 1 and cleaned_response['username'] != self.bot_username and cleaned_response['channel_name'] in self.synced_channels:
@@ -159,8 +182,23 @@ class Bot:
 
                                 send_message(self.irc_socket, channel, msg)
 
+                    # hack pencenters pop quizzes
+                    if cleaned_response['username'] == 'pencenter' and cleaned_response['message'].startswith('What is ') and cleaned_response['message'].split(' ')[3] == '+':
+                        # get the mesage
+                        split_message = cleaned_response['message'].split(' ')
+
+                        # perform the operation
+                        pq_ans = int(split_message[2]) + int(split_message[4]) * int(split_message[6])
+
+                    try:
+                        if int(cleaned_response['message'].strip()) == pq_ans:
+                            send_message(self.irc_socket, cleaned_response['channel_name'], f'!qed @{cleaned_response["username"]}')
+                            pq_ans = 'πa'
+                    except:
+                        pass
+
                     # reply to chat messages
-                    if cleaned_response['message'].startswith('$'):
+                    if cleaned_response['message'].startswith('$') or cleaned_response['message'].startswith('!'):
                         self.reply_to_message(cleaned_response)
         
         except KeyboardInterrupt:
@@ -192,7 +230,7 @@ class Bot:
             # send the default response based on the message
             send_message(self.irc_socket, channel_name, self.default_responses[message])
 
-        elif message.startswith('$saves'):
+        elif message.startswith('$saves') or message.startswith('!qed '):
             # make sure theyre a mod
             if not (mod_status or channel_name.lower() == username.lower()):
                 send_message(self.irc_socket, channel_name, 'you must be a mod to use this command')
@@ -204,9 +242,18 @@ class Bot:
                 return
 
             # increment the savecounter for that user
-            user_saves = self.increment_savecounter(message.split()[1])
-            send_message(self.irc_socket, channel_name, f'{message.split()[1]} has saved the day {user_saves} times.')
-        
+            try:
+                user_saves = self.increment_savecounter(message.split()[1], channel_name)
+            except Exception as e:
+                send_message(self.irc_socket, channel_name, e)
+                return
+            
+            # send an update message if pencenter hasn't already
+            if not message.startswith('!qed '):
+                send_message(self.irc_socket, channel_name, f'{message.split()[1]} has saved the day {user_saves[self.CHANNEL_COLUMNS.index(channel_name)]} times in {channel_name}.')
+            else:
+                print(f'{message.split()[1]} has saved the day {user_saves[self.CHANNEL_COLUMNS.index(channel_name)]} times in {channel_name}.')   
+
         elif message.startswith('$command add'):
             # make sure it's a correct input
             if len(message.split()) < 4:
@@ -299,27 +346,44 @@ class Bot:
             if selected_username.lower() in self.saves_counter:
                 user_saves = self.saves_counter[selected_username.lower()]
             else:
-                user_saves = 0
+                user_saves = [0] * (len(self.CHANNEL_COLUMNS) + 1)
 
-            send_message(self.irc_socket, channel_name, f'{message.split()[1]} has saved the day {user_saves} times.')
+            saves_str = f'{message.split()[1]} has saved the day {sum(user_saves)} times.'
 
-        else:
+            for i, channel_saves in enumerate(user_saves):
+                saves_str += f' {channel_saves} times in {self.CHANNEL_COLUMNS[i]},'
+
+            send_message(self.irc_socket, channel_name, saves_str[:-1] + '.')
+
+        elif message.startswith('$'):
             # catch all other messages
             send_message(self.irc_socket, channel_name, 'unknown command, type $commands for a list of commands')
 
-    def increment_savecounter(self, username):
+    def increment_savecounter(self, username, channel_name):
         '''increment the savecounter for that user
-        Parameters: saves_dict (dict), username (str)
+        Parameters: username (str), channel_name (str)
         Returns: users number of saves'''
 
+        # remove the @ to normalize usernames
         if username.startswith('@'):
             username = username[1:]
 
+        # make sure the channel name is in the list
+        if channel_name not in self.CHANNEL_COLUMNS:
+            raise Exception(f'{channel_name} not in self.CHANNEL_COLUMNS')
+
+        # make sure the username is in the dict, and add it if its not
         if username.lower() in self.saves_counter:
-            self.saves_counter[username] += 1
+            self.saves_counter[username][self.CHANNEL_COLUMNS.index(channel_name)] += 1
+            self.saves_counter[username][-1] += 1
             return self.saves_counter[username]
         else:
-            self.saves_counter[username] = 1
+            # create the user in the dict
+            self.saves_counter[username] = [0] * len(self.CHANNEL_COLUMNS)
+
+            # initialize the total and first save
+            self.saves_counter[username][self.CHANNEL_COLUMNS.index(channel_name)] = 1
+            self.saves_counter[username][-1] = 1
             return self.saves_counter[username]
             
 
@@ -339,16 +403,21 @@ def clean_response(response):
     # username
     username = tags['display-name']
 
+    # user statuses
     mod_status = tags['user-type']
+    vip_status = tags['vip']
+    subscriber_status = tags['subscriber']
 
     # pull the message
     message = response.split(f'PRIVMSG #{channel_name} :')[-1]
 
-    return {'message': message, 'username': username, 'channel_name': channel_name, 'mod_status': mod_status}          
+    return {'message': message, 'username': username, 'channel_name': channel_name, 'mod_status': mod_status, 'vip_status': vip_status, 'subscriber_status': subscriber_status}          
 
 def parse_message_tags(tags):
     '''parse the message tags
     '''
+
+    print(tags)
 
     # create an empty dictionary
     message_tags = {}
@@ -383,8 +452,9 @@ def open_sheet(filepath):
     # convert the sheets to dictionaries
     default_responses = dict(zip(command_outputs.col_values(1), command_outputs.col_values(2)))
     command_desc_dict = dict(zip(command_descriptions.col_values(1), command_descriptions.col_values(2)))
-    saves_ints = [int(i) for i in saves_counter.col_values(2)]
-    saves_dict = dict(zip(saves_counter.col_values(1), saves_ints))
+    saves_ints = np.array([[int(i) for i in saves_counter.col_values(j)[2:]] for j in range(2, 8)])
+    saves_dict = dict(zip(saves_counter.col_values(1)[2:], saves_ints.transpose()))
+
 
 
     return (command_desc_dict, default_responses, saves_dict)
@@ -393,9 +463,10 @@ def open_sheet(filepath):
 
 
 bot_username = 'en1gmabot'
-channel_names = ['en1gmabot', 'en1gmaunknown', 'dannyhighway']
+channel_names = ['en1gmabot', 'en1gmaunknown', 'dondoesmath', 'dannyhighway', 'etothe2ipi', 'pencenter', 'enstucky']
 
 my_bot = Bot(bot_username, channel_names)
 my_bot.join_chat()
 my_bot.run()
 
+ 
