7b2daa8fce2eecea2d566a8e3584867443eecddc
En1gmaUnknown <imfastasfrickboii@gmail.com>
2024-09-04 20:49:01 -0600

    added meri

--- productionBot.py
+++ productionBot.py
@@ -885,7 +885,7 @@ def open_sheet(filepath):
 
 
 bot_username = 'en1gmabot'
-channel_names = ['en1gmabot', 'en1gmaunknown', 'dondoesmath', 'dannyhighway', 'etothe2ipi', 'pencenter', 'enstucky', 'nsimplexpachinko', 'actualeducation']
+channel_names = ['en1gmabot', 'en1gmaunknown', 'dondoesmath', 'dannyhighway', 'etothe2ipi', 'pencenter', 'enstucky', 'nsimplexpachinko', 'actualeducation', 'meriwetherremark']
 
 my_bot = Bot(bot_username, channel_names)
 my_bot.join_chat()
